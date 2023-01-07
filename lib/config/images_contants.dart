4585a5519534be1464183147dc6ad4aee744f66f
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2023-01-06 21:34:52 -0300

    add images constants

---
+++ lib/config/images_contants.dart
@@ -0,0 +1,13 @@
+class ImagesConstants {
+  static const rules = 'lib/assets/images/image-rules.svg';
+  static const bgPentagon = 'lib/assets/images/bg-pentagon.svg';
+  static const bgTriangle = 'lib/assets/images/bg-triangle.svg';
+  static const logo = 'lib/assets/images/logo.svg';
+  static final icons = Icons();
+}
+
+class Icons {
+  static const close = 'lib/assets/images/icon-close.svg';
+  static const paper = 'lib/assets/images/icon-paper.svg';
+  static const rock = 'lib/assets/images/icon-rock.svg';
+}
