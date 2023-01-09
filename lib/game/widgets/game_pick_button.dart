5f34cc8b99db93b22534997c42b8d9a5bbe9a3d4
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2023-01-09 20:52:43 -0300

    style GamePickButton

--- lib/game/widgets/game_pick_button.dart
+++ lib/game/widgets/game_pick_button.dart
@@ -1,24 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-class GamePick extends StatelessWidget {
-  const GamePick({
+class GamePickButton extends StatelessWidget {
+  const GamePickButton({
     super.key,
-    this.pickImagePath,
-    this.gradientFirstColor,
-    this.gradientSecondColor,
-    this.action,
+    required this.pickImagePath,
+    required this.gradientFirstColor,
+    required this.gradientSecondColor,
+    required this.action,
   });
 
-  final String? pickImagePath;
-  final Color? gradientFirstColor;
-  final Color? gradientSecondColor;
-  final VoidCallback? action;
+  final String pickImagePath;
+  final Color gradientFirstColor;
+  final Color gradientSecondColor;
+  final VoidCallback action;
+  static const double circleSize = 108;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: action,
-      child: Container(),
+      child: Container(
+        width: circleSize,
+        height: circleSize,
+        decoration: BoxDecoration(
+          color: Colors.amber,
+          shape: BoxShape.circle,
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [gradientFirstColor, gradientSecondColor],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+            ),
+            child: SvgPicture.asset(pickImagePath),
+          ),
+        ),
+      ),
     );
   }
 }
