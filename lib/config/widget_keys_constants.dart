62ace4117dc284a0170686dcedac73e097d01220
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2023-01-10 09:45:49 -0300

    add widget keys constants

---
+++ lib/config/widget_keys_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class WidgetKeysConstants {
+  static const gamePickPaperButton = Key('player_game_pick_paper');
+  static const gamePickRockButton = Key('player_game_pick_rock');
+  static const gamePickRockScissor = Key('player_game_pick_scissor');
+  static const rulesModal = Key('rules_modal');
+}
