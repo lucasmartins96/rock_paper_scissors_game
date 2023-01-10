3d69c5ae3ee378d24f7122afe7e1768f81ceaa28
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2023-01-10 08:20:00 -0300

    add score cubit to game view tests

--- test/game/view/game_view_test.dart
+++ test/game/view/game_view_test.dart
@@ -3,24 +3,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend_mentor_rock_paper_scissors/game/game.dart';
+import 'package:frontend_mentor_rock_paper_scissors/score/score.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockGameBloc extends MockBloc<GameEvent, GameState> implements GameBloc {}
+
+class MockScoreCubit extends MockCubit<int> implements ScoreCubit {}
 
 void main() {
   const gamePickButtonPaper = Key('player_game_pick_paper');
   const gamePickButtonRock = Key('player_game_pick_rock');
   const gamePickButtonScissor = Key('player_game_pick_scissor');
   late GameBloc gameBloc;
+  late ScoreCubit scoreCubit;
 
   setUp(() {
     gameBloc = MockGameBloc();
+    scoreCubit = MockScoreCubit();
   });
 
   Future<void> pumpGameView(WidgetTester tester) {
     return tester.pumpWidget(
       MaterialApp(
-        home: BlocProvider.value(value: gameBloc, child: const GameView()),
+        home: MultiBlocProvider(
+          providers: [
+            BlocProvider.value(value: gameBloc),
+            BlocProvider.value(value: scoreCubit)
+          ],
+          child: const GameView(),
+        ),
       ),
     );
   }
@@ -35,6 +46,7 @@ void main() {
   group('GameView', () {
     testWidgets('renders initial Game view', (tester) async {
       when(() => gameBloc.state).thenReturn(const GameInitialState());
+      when(() => scoreCubit.state).thenReturn(0);
       await pumpGameView(tester);
       final rulesButton = find.widgetWithText(OutlinedButton, 'RULES');
 
