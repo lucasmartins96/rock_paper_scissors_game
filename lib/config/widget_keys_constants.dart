0e4f466f8e89993835413d4fe4854db72e39e215
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2023-01-11 15:15:22 -0300

    changes in WidgetKeysConstants
    - rename variable of game pick scissor button
    - add key to empty pick

--- lib/config/widget_keys_constants.dart
+++ lib/config/widget_keys_constants.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class WidgetKeysConstants {
   static const gamePickPaperButton = Key('player_game_pick_paper');
   static const gamePickRockButton = Key('player_game_pick_rock');
-  static const gamePickRockScissor = Key('player_game_pick_scissor');
+  static const gamePickScissorButton = Key('player_game_pick_scissor');
   static const rulesModal = Key('rules_modal');
   static const closeRulesModal = Key('close_rules_modal');
+  static const emptyPick = Key('empty_pick');
 }
