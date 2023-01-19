4daff10bd7f808cdac8497f9e78aa26c1f9ede19
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2023-01-19 14:36:25 -0300

    GameView refactor
    - add Header
    - add PageStepsWrapper and your widgets:
      - GamePicksBoard
      - PlayersPicksBoard and HomePick
    - add RulesButton

---
+++ lib/game/widgets/players_picks_board.dart
@@ -0,0 +1,73 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend_mentor_rock_paper_scissors/common.dart';
+import 'package:frontend_mentor_rock_paper_scissors/game/game.dart';
+
+class PlayersPicksBoard extends StatelessWidget {
+  const PlayersPicksBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final state = context.watch<GameBloc>().state;
+    final userPick = state.userPick;
+
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  GamePickButton(
+                    key: userPick!.buttonKey,
+                    pickImagePath: userPick.iconPath,
+                    gradientFirstColor: userPick.gradientBorderFirstColor,
+                    gradientSecondColor: userPick.gradientBorderSecondColor,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: Text(
+                      'YOU PICKED',
+                      style: TextStyle(
+                        color: Colors.white,
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // TODO: Avaliar necessidade do BlocSelector
+            BlocSelector<GameBloc, GameState, GamePick?>(
+              selector: (state) => state.homePick,
+              builder: (context, homePick) {
+                return Expanded(
+                  child: Column(
+                    children: [
+                      HomePick(homePick: homePick),
+                      const Padding(
+                        padding: EdgeInsets.only(top: 20),
+                        child: Text(
+                          'THE HOUSE PICKED',
+                          style: TextStyle(
+                            color: Colors.white,
+                            letterSpacing: 1,
+                            fontWeight: FontWeight.w800,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+        AnimatedGameResult(isUserWin: state.isUserWin),
+      ],
+    );
+  }
+}
---
+++ lib/game/widgets/rules_button.dart
@@ -0,0 +1,42 @@
+import 'package:frontend_mentor_rock_paper_scissors/common.dart';
+import 'package:frontend_mentor_rock_paper_scissors/config/config.dart';
+import 'package:frontend_mentor_rock_paper_scissors/game/game.dart';
+
+class RulesButton extends StatelessWidget {
+  const RulesButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 48),
+      child: OutlinedButton(
+        onPressed: () {
+          showModalBottomSheet<void>(
+            context: context,
+            isScrollControlled: true,
+            builder: (_) => const RulesModal(
+              key: WidgetKeysConstants.rulesModal,
+            ),
+          );
+        },
+        style: OutlinedButton.styleFrom(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+          side: const BorderSide(width: 1.5, color: Colors.white),
+        ),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Text(
+            'RULES',
+            style: TextStyle(
+              fontSize: 16,
+              letterSpacing: 1,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
