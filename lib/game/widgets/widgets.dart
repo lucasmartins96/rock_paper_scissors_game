8dcab9cfb5e4f335739bb576c95354ac17344d1a
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2023-01-19 14:42:51 -0300

    update widgets barrel file

--- lib/game/widgets/widgets.dart
+++ lib/game/widgets/widgets.dart
@@ -1,1 +1,10 @@
+export 'animated_game_result.dart';
 export 'game_pick_button.dart';
+export 'game_pick_empty.dart';
+export 'game_picks_board.dart';
+export 'header.dart';
+export 'home_pick.dart';
+export 'page_steps_wrapper.dart';
+export 'players_picks_board.dart';
+export 'rules_button.dart';
+export 'rules_modal.dart';
